8965ca97de794704e9373777a4a80780be7b846e
a166527 <62819737+a166527@users.noreply.github.com>
2020-12-13 17:38:10 -0300

    Update Financeiro.java

--- src/Financeiro.java
+++ src/Financeiro.java
@@ -1,3 +1,13 @@
+/*-----------------MC322---------------*/
+/* Projeto: eComputerStore             */
+/* Grupo: Angelo H Barbosa      166527 */
+/*        Franscisco G Pena     234589 */
+/*        Guilherme H Pascon    235980 */
+/*        Luís Felipe L B Silva 156441 */
+/*                                     */
+/* Data: dez 2020                      */
+/*-------------------------------------*/
+
 public class Financeiro {
     private static float caixaTotal;
 
