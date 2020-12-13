1f786f28550297824192aee3480d1042adc7f5b8
a166527 <62819737+a166527@users.noreply.github.com>
2020-12-13 19:36:45 -0300

    Update Estoque.java

--- src/Estoque.java
+++ src/Estoque.java
@@ -10,23 +10,76 @@
 
 import java.util.ArrayList;
 
+/*----- Classe Estoque -----*/
+/* Objetivo: */
+/*--------------------------*/
 public class Estoque {
     
     private static quantEstoque = 0;
     
+    private int idEstoque;
     private ArrayList<Integer> estoqueProdutos;
     private ArrayList<Produto> produtos;
 
-    public Estoque(){
+    /* Construtor */
+    public Estoque(int id){
+        this.id = id;
         this.estoqueProdutos = new ArrayList<Integer>;
         this.produtos = new ArrayList<Produto>; 
     }
     
-    public static ArrayList<Integer> getEstoqueProdutos() {
+    /* Metodos */
+    public boolean addProdutoEstoque(Produto produto, Estoque estoque){
+        if(!(estoque.getProdutos().contains(produto))){
+            estoque.getProdutos().add(produto);
+            return true;
+        }
+        else
+            return false;
+    }
+    public boolean removeProdutoEstoque(Produto produto, Estoque estoque){
+        if(estoque.getProdutos().contains(produto)){
+            estoque.getProdutos().remove(produto);
+            return true;
+        }
+        else
+            return false;
+    }
+    
+    
+    public boolean addQuantEstoque(int quant, Produto produto, Estoque estoque){
+        if(estoque.getProdutos.contains(produto)){
+            estoque.getEstoqueProdutos().get(produto.getId()) += quant;
+            return true;
+         }
+         else
+            return false;
+    }
+    public boolean removeQuantEstoque(int quant, Produto produto, Estoque estoque){
+        if(estoque.getProdutos.contains(produto)){
+            if(estoque.getEstoqueProdutos().get(produto.getId()) >= quant){
+                estoque.getEstoqueProdutos().get(produto.getId()) -= quant;
+                return true;
+            }
+            else
+                return false;
+        }
+        else
+            return false;
+    }
+    
+    /* Getters e Setters */
+    public ArrayList<Integer> getEstoqueProdutos() {
         return estoqueProdutos;
     }
-
-    public static void setEstoqueProdutos(ArrayList<Integer> estoqueProdutos) {
-        Estoque.estoqueProdutos = estoqueProdutos;
+    public void setEstoqueProdutos(ArrayList<Integer> estoqueProdutos) {
+        this.estoqueProdutos = estoqueProdutos;
+    }
+    
+    public ArrayList<Produto> getProdutos() {
+        return produtos;
+    }
+    private void setProdutos(ArrayList<Produto> produtos) {
+        this.produtos = produtos;
     }
 }
