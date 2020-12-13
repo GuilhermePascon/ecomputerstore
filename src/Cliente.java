a1c516ca51b93166aa81e891259d21a2b6a576b8
a166527 <62819737+a166527@users.noreply.github.com>
2020-12-13 17:43:33 -0300

    Update Cliente.java

--- src/Cliente.java
+++ src/Cliente.java
@@ -13,15 +13,15 @@ import java.util.ArrayList;
 /*----- Classe Cliente -----*/
 /* Objetivo: */
 /*--------------------------*/
-
-
 public class Cliente {
     private static int quantCliente = 0;
+    
     private float saldo;
     private ArrayList<Produto> carrinho;
     private int id;
     private String nome;
 
+    /* Construtor */
     public Cliente(float saldo, ArrayList<Produto> carrinho, String nome) {
         quantCliente++;
         this.id = getQuantCliente();
@@ -31,28 +31,26 @@ public class Cliente {
         this.nome = nome;
     }
 
+    /* Metodos Estaticos */
     public static int getQuantCliente() {
         return quantClient;
     }
-
     public static void setQuantClient(int quantCliente) {
         Cliente.quantClient = quantClient;
     }
     
     
-
+    /* Getters e Setters */
     public float getSaldo() {
         return saldo;
     }
-
-    public void setSaldo(float saldo) {
+    private void setSaldo(float saldo) {
         this.saldo = saldo;
     }
 
     public ArrayList<Produto> getCarrinho() {
         return carrinho;
     }
-
     public void setCarrinho(ArrayList<Produto> carrinho) {
         this.carrinho = carrinho;
     }
@@ -60,16 +58,14 @@ public class Cliente {
     public int getId() {
         return id;
     }
-
-    public void setId(int id) {
+    private void setId(int id) {
         this.id = id;
     }
 
     public String getNome() {
         return nome;
     }
-
-    public void setNome(String nome) {
+    private void setNome(String nome) {
         this.nome = nome;
     }
 }
