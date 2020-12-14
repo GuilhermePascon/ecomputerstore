25852086009cc12ad67210cfac92d57e724adae5
GFranciscoP <64993997+GFranciscoP@users.noreply.github.com>
2020-12-14 18:28:39 -0300

    Update Financeiro.java

--- src/Financeiro.java
+++ src/Financeiro.java
@@ -10,6 +10,8 @@
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
 public class Financeiro {
     private static float caixaTotal;
@@ -25,6 +27,20 @@ public class Financeiro {
             margens.add(p.get(i).getMargemLucro());
         }
         return st.media(margens);
+    }
+
+    public ArrayList<Integer> maisVendido(){
+        Estatistica st = new Estatistica();
+        ArrayList<Pedido> pedidos = Comercial.getListaPedidos();
+        ArrayList<Integer> qnt_ped = new ArrayList<Integer>(Collections.nCopies(Produto.getQuantTotalProduto(), 0));
+        for (int i = 0; i < pedidos.size(); i++){
+            ArrayList<Produto> prod = pedidos.get(i).getCarrinho();
+            for (int j = 0; j < prod.size(); j++){
+                int aux = prod.get(j).getId();
+                qnt_ped.add(aux, qnt_ped.get(aux) + 1); // Adiciona 1 a posição id de qnt_ped
+            }
+        }
+        return st.maisVendido(qnt_ped); // Retorna id do produto mais vendido
     }
 
     /* Getter e Setter */
