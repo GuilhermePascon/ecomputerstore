e14b926d13196688f7f2d5cb8f7fe28ecd5bdb10
GFranciscoP <64993997+GFranciscoP@users.noreply.github.com>
2020-12-14 17:33:50 -0300

    Update Estatistica.java

--- src/Estatistica.java
+++ src/Estatistica.java
@@ -8,9 +8,30 @@
 /* Data: dez 2020                      */
 /*-------------------------------------*/
 
+import java.util.ArrayList;
+
 public class Estatistica {
     private static float dolar;
 
+    /* Métodos */
+    public int maisVendido(ArrayList<Integer> produtos){
+        int qnt_vendas = 0;
+        int maisVendidos;
+        for (int i = 0; i < produtos.size(); i++);
+        // Incompleto (Necessita alinhar com Estoque)
+        return 0;
+    }
+
+    public float media(ArrayList<Float> margens){
+        float soma = 0;
+        int qnt = 0;
+        for (int i = 0; i < margens.size(); i++){
+            soma = soma + margens.get(i);
+            qnt = qnt + 1;
+        }
+            return soma/qnt;
+    }
+    /* Getters e Setters */
     public static float getDolar() {
         return dolar;
     }
@@ -18,4 +39,6 @@ public class Estatistica {
     public static void setDolar(float dolar) {
         Estatistica.dolar = dolar;
     }
+
+
 }
